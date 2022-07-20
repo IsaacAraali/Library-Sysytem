464e6d4f331fe710ef5d0da1909fecc03f4d86ea
Musemeza Murungi Isaac <musemezamurungiisaac@gmail.com>
2022-07-20 08:36:48 +0000

    Update settings.py

--- new1/settings.py
+++ new1/settings.py
@@ -137,7 +137,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'),
-#)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
