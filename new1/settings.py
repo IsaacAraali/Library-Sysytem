55a794187ee2e0940c465370013debcffcefe1d0
Musemeza Murungi Isaac <musemezamurungiisaac@gmail.com>
2022-07-20 08:06:54 +0000

    Update settings.py

--- new1/settings.py
+++ new1/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
