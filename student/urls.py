4d0fd2c66087c24549c4369cf84aa01579d657e2
Musemeza Murungi Isaac <musemezamurungiisaac@gmail.com>
2022-08-01 09:44:11 +0000

    Update urls.py

--- student/urls.py
+++ student/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('book_list', views.book_list, name='book_list'),
     path('borrowed_books', views.borrowed_books, name='borrowed_books'),
     path('books/<str:pk>', views.one_book, name='book'),
+    path('search_book', views.search_book, name='search_book1'),
 
 ]
