b88528b6aa3bb941a7ac78bbbb6dddcc51225349
NAKAZIGO OLIVIA <nakazigoolivia@gmail.com>
2022-08-24 14:37:11 +0000

    Update student/urls.py

--- student/urls.py
+++ student/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('book_list', views.book_list, name='book_list'),
-    path('requested_books', views.borrowed_books, name='borrow_books'),
+    path('request_books', views.borrowed_books, name='borrow_books'),
     path('books/<str:pk>', views.one_book, name='book'),
     path('search_book', views.search_book, name='search_book1'),
 
