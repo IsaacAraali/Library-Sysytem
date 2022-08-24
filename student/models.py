6c30bd67cc9cf5a7a20f5497e88e097c80985bc2
Opio Douglas <dopio8081@gmail.com>
2022-08-24 13:45:02 +0000

    Update student/models.py, student/urls.py

--- student/models.py
+++ student/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bookrequest(models.Model):
+    student_name =models.CharField(max_length=255)
+    student_number = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
